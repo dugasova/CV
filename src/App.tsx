6f76566fb7dfcf4cdb2bc91a7bae918b4af274bc
Nataly Dugasova <dugasova@gmail.com>
2026-06-23 12:05:52 +0000

    feat: initialize new React project with Vite, TypeScript, and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+export default function App() {
+  return (
+    <div>
+      <h1>Natalia Dugasova</h1>
+      <p>Software Engineer</p>
+      <p>Frontend</p>
+      <p>React</p>
+
+    </div>
+  )
+}
